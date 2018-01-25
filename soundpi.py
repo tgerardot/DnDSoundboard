87bab660fa0f8ec42b5ef24978bdbb0c6587770d
tgerardot <tgerardot@masterspas.com>
2018-01-25 17:03:22 -0500

    Added Misc as first category. Changed page_number to category

--- soundpi.py
+++ soundpi.py
@@ -15,19 +15,104 @@ if page_number >= 2:
     page_number = 0
 
 while page_number == 0:
-    if (GPIO.input(18) == False):
-             pygame.mixer.music.load('sounds/spells/acid-splash.mp3')
-             pygame.mixer.music.play(0)
-             page_number += 1
-             print (page_number)
-             time.sleep(0.2)
-
-while page_number == 1:
-    if (GPIO.input(18) == False):
+    if (GPIO.input(4) == False):
              pygame.mixer.music.load('sounds/misc/airhorn.mp3')
              pygame.mixer.music.play(0)
-             print (page_number)
              time.sleep(0.2)
+    if (GPIO.input(17) == False):
+             pygame.mixer.music.load('sounds/misc/alert.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(27) == False):
+             pygame.mixer.music.load('sounds/misc/dragon-roar.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(22) == False):
+             pygame.mixer.music.load('sounds/misc/glug-glug.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(10) == False):
+             pygame.mixer.music.load('sounds/misc/holy.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(9) == False):
+             pygame.mixer.music.load('sounds/misc/horn-of-war.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)                                                                 
+    if (GPIO.input(11) == False):
+             pygame.mixer.music.load('sounds/misc/jump.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(5) == False):
+             pygame.mixer.music.load('sounds/misc/phantom.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(6) == False):
+             pygame.mixer.music.load('sounds/misc/sneak.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(13) == False):
+             pygame.mixer.music.load('sounds/misc/zombie-crowd.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(19) == False):
+             pygame.mixer.music.load('sounds/misc/zombie-hit.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(26) == False):
+             pygame.mixer.music.load('sounds/misc/zombie-hit.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)                                      
+
+while page_number == 1:
+    if (GPIO.input(4) == False):
+             pygame.mixer.music.load('sounds/battle/arrow-swoosh-hit.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(17) == False):
+             pygame.mixer.music.load('sounds/battle/impale.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(27) == False):
+             pygame.mixer.music.load('sounds/battle/slap.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    if (GPIO.input(22) == False):
+             pygame.mixer.music.load('sounds/battle/victory.mp3')
+             pygame.mixer.music.play(0)
+             time.sleep(0.2)
+    # if (GPIO.input(10) == False):
+    #          pygame.mixer.music.load('sounds/battle/.mp3')
+    #          pygame.mixer.music.play(0)
+    #          time.sleep(0.2)
+    # if (GPIO.input(9) == False):
+    #          pygame.mixer.music.load('sounds/battle/.mp3')
+    #          pygame.mixer.music.play(0)
+    #          time.sleep(0.2)                                                                 
+    # if (GPIO.input(11) == False):
+    #          pygame.mixer.music.load('sounds/battle/.mp3')
+    #          pygame.mixer.music.play(0)
+    #          time.sleep(0.2)
+    # if (GPIO.input(5) == False):
+    #          pygame.mixer.music.load('sounds/battle/.mp3')
+    #          pygame.mixer.music.play(0)
+    #          time.sleep(0.2)
+    # if (GPIO.input(6) == False):
+    #          pygame.mixer.music.load('sounds/battle/.mp3')
+    #          pygame.mixer.music.play(0)
+    #          time.sleep(0.2)
+    # if (GPIO.input(13) == False):
+    #          pygame.mixer.music.load('sounds/battle/.mp3')
+    #          pygame.mixer.music.play(0)
+    #          time.sleep(0.2)
+    # if (GPIO.input(19) == False):
+    #          pygame.mixer.music.load('sounds/battle/.mp3')
+    #          pygame.mixer.music.play(0)
+    #          time.sleep(0.2)
+    # if (GPIO.input(26) == False):
+    #          pygame.mixer.music.load('sounds/battle/.mp3')
+    #          pygame.mixer.music.play(0)
+    #          time.sleep(0.2)
              if page_number >= 2:
                  page_number = 0
 
