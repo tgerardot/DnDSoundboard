162bb0a1b5a7a8ee26db4caed53a5ddeb346cfef
tgerardot <tgerardot@masterspas.com>
2018-02-05 12:09:42 -0500

    added shutdown script to be run on pi startup. Pin 21

---
+++ shutdown.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import os
+import time
+
+gpio_pin_number=21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio_pin_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    GPIO.wait_for_edge(gpio_pin_number, GPIO.FALLING)
+    time.sleep(2)
+    if GPIO.input(gpio_pin_number) == 0:
+        break
+
+os.system("sudo shutdown -h now")
